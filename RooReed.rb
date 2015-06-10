0d0e3e5fc5bbcf1de3428776d38ea776c68308c4
Sergio Monsalve <smonsalve@gmail.com>
2015-06-10 08:54:41 -0500

    Clean Test

--- RooReed.rb
+++ RooReed.rb
@@ -1,16 +1,16 @@
 require 'roo'
 
-xlsx = Roo::Excelx.new("./Canal_Endemico_diferentes.xlsx")
+xlsx = Roo::Excelx.new("./Entradas.xlsx")
 # xlsx.each_row_streaming do |row|
 #   puts row.inspect # Array of Excelx::Cell objects
 # end
 
 
-#xlsx.info
+#puts xlsx.info
 #puts xlsx.sheets
 #puts xlsx.sheet("2009").row(1) 
 row = []
-hoja1 = xlsx.sheet("2009")
+hoja1 = xlsx.sheet("input")
 header = hoja1.row(1)
 (2..hoja1.last_column).each do |i|
 	row = Hash[[header,hoja1.row(i)].transpose]
