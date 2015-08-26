8e94243edc654569125a3602c87c04d06ad69892
Sergio Monsalve <smonsalve@gmail.com>
2015-08-26 10:39:13 -0500

    Informe Semana 13

--- src/roo_reed.rb
+++ src/roo_reed.rb
@@ -1,5 +1,14 @@
 #!/usr/bin/env ruby
 
+# years.each do |y|
+#   puts y
+# end
+
+# years.each {|a| puts a.semanas.count}
+
+# header = hoja1.row(0)
+# puts header
+
 require 'roo'
 require_relative 'epi_year'
 require 'easystats'
@@ -12,25 +21,16 @@ desviacion = []
 ic95_superior = []
 ic95_inferior = []
 
+puts "Leyendo base de datos... "
 xlsx = Roo::Excelx.new("../input/Entradas.xlsx")
 hoja1 = xlsx.sheet("input")
-# header = hoja1.row(0)
-# puts header
-
 
 (hoja1.first_row..hoja1.last_row).each do |r|
   fila = hoja1.row(r)
   e =  EpiYear.new(fila[0], fila[54])
   (1..52).each { |a| e.semanas << fila[a].to_i }
-  #puts e
   years << e
 end
-
-# years.each do |y|
-#  	puts y
-# end
-
-# years.each {|a| puts a.semanas.count}
 
 a = []
 years.each do |y|
@@ -39,7 +39,7 @@ end
 
 b = Matrix.rows(a)
 
-
+puts "Calculando datos estadisticos... "
 ((0)..(b.column_count-1)).each do |col|
   avrg = b.column(col).to_a.average 
   stdv = b.column(col).to_a.standard_deviation 
@@ -49,7 +49,9 @@ b = Matrix.rows(a)
   ic95_inferior << avrg - (2.02*(stdv/Math.sqrt(5)))
 end
 
-
-#puts ic95_inferior
-puts promedio
-puts ic95_superior
+puts "Promedio de casos"
+print promedio
+puts "Indice de confianza Superior"
+print ic95_inferior
+puts "Indice de confianza Inferior"
+print ic95_superior
